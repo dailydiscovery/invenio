1de9f8a144105a02f878120da0369beeea0b692c
Simeon F. Willbanks <sfw@simeonfosterwillbanks.com>
2013-12-27 14:02:26 -0800

    Add skeleton tests

--- test/invenio/pull_request_test.rb
+++ test/invenio/pull_request_test.rb
@@ -3,4 +3,19 @@ require "test_helper"
 class Invenio::PullRequestTest < MiniTest::Unit::TestCase
   def setup
   end
+
+  def test_can_accept_with_good_files
+  end
+
+  def test_cant_accept_with_bad_files
+  end
+
+  def test_merge_when_acceptable
+  end
+
+  def test_comment_when_not_acceptable
+  end
+
+  def test_dont_comment_when_not_acceptable_but_already_commented
+  end
 end
